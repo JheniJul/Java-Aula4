3b7489a480e827fc8b1c1b4cd8d4a3cd69db54fe
JheniJul <108576475+JheniJul@users.noreply.github.com>
2023-09-03 21:30:41 -0300

    Add files via upload

---
+++ Algoritmo358/src/main/java/com/mycompany/algoritmo358/Algoritmo358.java
@@ -0,0 +1,45 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.algoritmo358;
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author sonha
+ */
+public class Algoritmo358 {
+
+    public static void main(String[] args) {
+        int a;
+        float lucro;
+        Produto pr = new Produto();
+        
+        int tdez = 0; //totlucromenor10
+        int tvinte = 0; // totlucromenor20
+        int tmaior = 0; // totlucromaior20
+        
+        for(a=0; a<100; a++){
+            pr.pcompra[a]=Float.parseFloat(JOptionPane.showInputDialog("\nPreco de compra: ")); 
+            pr.pvenda[a]=Float.parseFloat(JOptionPane.showInputDialog("\nPreco de venda: ")); 
+        }
+        
+        for(a=0; a<100; a++){
+            lucro = pr.pvenda[a] - pr.pcompra[a];
+            
+            if(lucro<10.0){ tdez++; }
+            else{
+                if(lucro <= 20.0){ tvinte++; }
+                else{ tmaior++; }
+            }
+        }
+    
+        System.out.println("\nTotal de mercadorias com lucro < 10%: " +tdez);
+        System.out.println("\nTotal de mercadorias com 10% <= lucro <= 20%: " +tvinte);
+        System.out.println("\nTotal de mercadorias com lucro > 20%: " +tmaior);
+        
+        System.out.println("\n");
+    }   
+    
+}
---
+++ Algoritmo358/src/main/java/com/mycompany/algoritmo358/Produto.java
@@ -0,0 +1,14 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.algoritmo358;
+
+/**
+ *
+ * @author sonha
+ */
+public class Produto {
+    float pcompra[] = new float[100];
+    float pvenda[] = new float[100];
+}
